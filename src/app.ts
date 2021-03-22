ce711d4661ee43409917b55bb8bb1d59b1cff3e2
Richard Zampieri <br.developer@gmail.com>
2021-03-21 23:25:31 -0700

    BaseController provider class added and response helper functions

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+const compression = require('compression');
 import { router } from './router';
 
 // initialize express
 const app = express();
 
 // add middlewares
+app.use(compression());
 app.use(express.json());
 app.use(router);
 
--- src/useCases/player/create/CreatePlayer.Controller.ts
+++ src/useCases/player/create/CreatePlayer.Controller.ts
@@ -1,19 +1,22 @@
 import { Request, Response } from "express";
+import { BaseController } from "@/providers/controller/BaseController";
 import { CreatePlayerUseCase } from "./CreatePlayer.UseCase";
 
-export class CreatePlayerController {
+export class CreatePlayerController extends BaseController {
     
-    constructor(private createPlayerUseCase: CreatePlayerUseCase) {}
-
+    constructor(private createPlayerUseCase: CreatePlayerUseCase) {
+        super();
+    }
+    
     async handleRequest(request: Request, response: Response): Promise<Response> {
 
         const { name, email, faction } = request.body;
 
         try {
             await this.createPlayerUseCase.execute({name, email, faction});
-            return response.status(201).send();
+            return  this.created(response); 
         } catch (error) {
-            return response.status(400).json({errorMessage: error.message || 'Internal server error!'});
+            return this.fail(response, error || 'Internal server error!');
         }
-    }
+    }   
 }
