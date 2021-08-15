63c1e170ec937c4a0663a1807adb22428cf04a96
Lucas Santos <lucashfreitas1@gmail.com>
2021-08-15 13:50:54 +1200

    missing commits

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -57,7 +57,7 @@ export const Form: React.FC = () => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
         >
-          Sign In
+          Save Player
         </button>
       </form>
     </div>
