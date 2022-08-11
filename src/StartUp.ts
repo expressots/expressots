01ec97851d575835a7fd5146db4d93781bf98448
Richard Zampieri <br.developer@gmail.com>
2022-08-11 01:17:53 -0700

    Updated version of typedoc

--- src/StartUp.ts
+++ src/StartUp.ts
@@ -1,3 +1,9 @@
+/**
+ * Class -> StartUp
+ * This is the entry point of the application. This class is responsible for bootstrapping the application,
+ * passing a message to console defining environment, version, app. name and port to be used.
+ * 
+ */
 import "reflect-metadata";
 
 import { appConfig } from "AppConfig";
@@ -7,13 +13,13 @@ import { ServerInversifyContainer } from "@providers/inversify/Container.Provide
 const PORT = Env.Server.DEFAULT_PORT;
 
 appConfig.listen(PORT, async () => {
-    ServerInversifyContainer.Init({
-        appName: Env.Server.APP_NAME,
-        appVersion: Env.Server.APP_VERSION,
-        timezone: Env.Server.TIMEZONE,
-        adminEmail: Env.Server.ADMIN_EMAIL,
-        language: Env.Server.DEFAULT_LANGUAGE,
-        environment: Env.Server.ENVIRONMENT,
-        port: PORT
-    });
+  ServerInversifyContainer.Init({
+    appName: Env.Server.APP_NAME,
+    appVersion: Env.Server.APP_VERSION,
+    timezone: Env.Server.TIMEZONE,
+    adminEmail: Env.Server.ADMIN_EMAIL,
+    language: Env.Server.DEFAULT_LANGUAGE,
+    environment: Env.Server.ENVIRONMENT,
+    port: PORT,
+  });
 });
