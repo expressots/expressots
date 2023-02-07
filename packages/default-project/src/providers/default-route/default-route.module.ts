aec2a63f942264f119068dae513ad3183b19d0fc
Richard Zampieri <rc_scott48@hotmail.com>
2023-02-06 19:58:05 -0800

    Changed default-route controller folder naming

--- packages/default-project/src/providers/default-route/default-route.module.ts
+++ packages/default-project/src/providers/default-route/default-route.module.ts
@@ -1,5 +1,5 @@
 import { CreateModule } from '@expressots/core';
-import { DefaultRouterController } from './default-route';
+import { DefaultRouterController } from './default-route.controller';
 
 const BaseRouterContainerModule = CreateModule([
     // Add your modules here
