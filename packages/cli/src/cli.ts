9164b6babdaee7671c5455c66e1ae4bd62baa434
Richard Zampieri <rc_scott48@hotmail.com>
2023-02-21 21:16:41 -0800

    fix: add npm as standard for tooling

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -6,8 +6,7 @@ import { createProject } from "./new";
 
 yargs(hideBin(process.argv))
 	.example("$0 new <project-name>", "Create a new Expresso TS project")
-	.command(createProject())
-	// .command(importProject())
+	.command(createProject() as any)
 	.demandCommand(1, "You need at least one command before moving on")
 	.epilog("For more information, visit https://expresso-ts.com.")
 	.help()
