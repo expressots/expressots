3caec232fa93cd25c0f6dae07b417e2b76b364c8
Felipe Fontana <felipefontanaf@gmail.com>
2023-03-18 00:04:25 -0400

    Change tester runner (#2)
    * Update README and Add tests
    
    * Change tester runner -  ts-jest to swc/jest

--- packages/boost-ts/jest.config.js
+++ packages/boost-ts/jest.config.js
@@ -1,5 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
   testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
 };
