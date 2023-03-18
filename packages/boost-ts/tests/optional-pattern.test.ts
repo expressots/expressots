838c4711e760c9dc8f322e9f8ed2fab1585acccb
Felipe Fontana <felipefontanaf@gmail.com>
2023-03-18 00:44:17 -0400

    Add optional pattern tests (#3)

---
+++ packages/boost-ts/tests/optional-pattern.test.ts
@@ -0,0 +1,21 @@
+import { Some, None, Optional } from "../src/optional-pattern";
+import { match } from '../src/match-pattern';
+
+
+describe('testing optional pattern function', () => {
+  const v1: Optional<number> = Some(1);
+  const v2: Optional<number> = None();
+
+  test('optional pattern', () => {
+    expect(match(v1, {
+      Some: (x) => x + 1,
+      None: 0,
+    })).toBe(2);
+
+    expect(match(v2, {
+      Some: (x) => x + 1,
+      None: 0,
+    })).toBe(0);
+  })
+
+})
