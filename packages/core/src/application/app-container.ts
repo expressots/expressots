e9b35bc0812f57ef1ced0998b0e8138fcf50f9a8
Richard Zampieri <br.developer@gmail.com>
2023-05-14 14:23:58 -0700

    Feature/container binding (#30)
    * feat: add singleton,transient,scope to container, providers, entities
    
    * fix: update sponsor info on package

--- packages/core/src/application/app-container.ts
+++ packages/core/src/application/app-container.ts
@@ -1,4 +1,4 @@
-import { Container, ContainerModule } from "inversify";
+import { BindingScopeEnum, Container, ContainerModule, interfaces } from "inversify";
 import { buildProviderModule, provide } from "inversify-binding-decorators";
 
 /**
@@ -8,21 +8,48 @@ import { buildProviderModule, provide } from "inversify-binding-decorators";
 @provide(AppContainer)
 class AppContainer {
 
-    private container: Container;
-
-    /**
-     * Constructs a new instance of the AppContainer class.
-     */
-    constructor() { }
-
+    private container!: Container;
     /**
      * Creates and configures a new dependency injection container.
      * @param modules - An array of ContainerModule instances to load into the container.
+     * @param defaultScope - The default scope to use for bindings. Scoped (Request) by default, but offers Singleton and Transient as well.
      * @returns The configured dependency injection container.
      */
-    public create(modules: ContainerModule[]): Container {
-        this.container = new Container();
+    public create(
+        modules: ContainerModule[],
+        defaultScope: interfaces.BindingScope = BindingScopeEnum.Request,
+    ): Container {
+        this.container = new Container({
+            autoBindInjectable: true,
+            defaultScope,
+        });
+
         this.container.load(buildProviderModule(), ...modules);
+
+        return this.container;
+    }
+
+    /**
+     * Retrieves the binding dictionary of the container.
+     * @returns The binding dictionary of the container.
+     */
+    public getBindingDictionary(): Map<any, any> {
+        return this.container["_bindingDictionary"]._map;
+    }
+
+    /**
+     * Retrieves the container options.
+     * @returns The container options.
+     */
+    public getContainerOptions(): interfaces.ContainerOptions {
+        return this.container["options"];
+    }
+
+    /**
+     * Retrieves the container.
+     * @returns The container.
+     */
+    public get Container(): Container {
         return this.container;
     }
 }
---
+++ packages/core/src/decorator/scope-binding.ts
@@ -0,0 +1,11 @@
+import { fluentProvide } from "inversify-binding-decorators";
+
+const provideSingleton = (identifier: any) => {
+    return fluentProvide(identifier).inSingletonScope().done();
+};
+
+const provideTransient = (identifier: any) => {
+    return fluentProvide(identifier).inTransientScope().done();
+};
+
+export { provideSingleton, provideTransient };
