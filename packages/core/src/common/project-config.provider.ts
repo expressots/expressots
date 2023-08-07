5a6549e2624ae38b3decc613038076e3c3d01dde
Vitor Caminha <vitorcaminha@gmail.com>
2023-08-07 00:25:43 -0300

    feat: add prisma provider configuration types in ExpressoConfig

--- packages/core/src/common/project-config.provider.ts
+++ packages/core/src/common/project-config.provider.ts
@@ -5,12 +5,22 @@ const enum Pattern {
   CAMEL_CASE = "camelCase",
 }
 
+interface IProviders {
+	prisma?: {
+		schemaName: string;
+		schemaPath: string;
+		entitiesPath: string;
+		entityNamePattern: string;
+	}
+}
+
 /**
  * The configuration object for the Expresso CLI.
  *
  * @property {Pattern} scaffoldPattern - The pattern to use when scaffolding files.
  * @property {string} sourceRoot - The root directory for the source files.
  * @property {boolean} opinionated - Whether or not to use the opinionated configuration.
+ * @property {IProviders} providers - Specific configuration for each provider added.
  *
  * @see [ExpressoConfig](https://expresso-ts.com/docs)
  */
@@ -18,6 +28,7 @@ interface ExpressoConfig {
   scaffoldPattern: Pattern;
   sourceRoot: string;
   opinionated: boolean;
+	providers?: IProviders;
 }
 
 export { ExpressoConfig, Pattern };
