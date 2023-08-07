81eaf930e0915c1e10d117fe41983782b2e4b5bf
Richard Zampieri <br.developer@gmail.com>
2023-08-07 02:19:16 -0700

    fix: remove version update and add doc

--- packages/core/src/common/project-config.provider.ts
+++ packages/core/src/common/project-config.provider.ts
@@ -1,3 +1,11 @@
+/**
+ * Enum representing different string patterns.
+ * 
+ * - LOWER_CASE: Represents strings in all lowercase letters. E.g. "hello"
+ * - KEBAB_CASE: Represents strings separated by hyphens. E.g. "hello-world"
+ * - PASCAL_CASE: Represents strings where the first letter of each word is capitalized. E.g. "HelloWorld"
+ * - CAMEL_CASE: Represents strings where the first letter of the first word is lowercase and the first letter of subsequent words are capitalized. E.g. "helloWorld"
+ */
 const enum Pattern {
   LOWER_CASE = "lowercase",
   KEBAB_CASE = "kebab-case",
@@ -5,6 +13,14 @@ const enum Pattern {
   CAMEL_CASE = "camelCase",
 }
 
+/**
+ * Interface describing configuration specific to Prisma provider.
+ *
+ * @property {string} schemaName - The name of the Prisma schema.
+ * @property {string} schemaPath - The path to the Prisma schema file.
+ * @property {string} entitiesPath - The path to the directory containing entities.
+ * @property {string} entityNamePattern - The naming convention to use for entity names.
+ */
 interface IProviders {
 	prisma?: {
 		schemaName: string;
@@ -22,7 +38,7 @@ interface IProviders {
  * @property {boolean} opinionated - Whether or not to use the opinionated configuration.
  * @property {IProviders} providers - Specific configuration for each provider added.
  *
- * @see [ExpressoConfig](https://expresso-ts.com/docs)
+ * @see [Doc](https://doc.expresso-ts.com/)
  */
 interface ExpressoConfig {
   scaffoldPattern: Pattern;
