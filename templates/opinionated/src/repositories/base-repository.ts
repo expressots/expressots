29541f997e47c8ac5f8cafbb0195c34c904454f4
Richard Zampieri <br.developer@gmail.com>
2023-09-05 17:08:30 -0700

    fix: add generic in memory db and virtual table analysis (#86)

--- templates/opinionated/src/repositories/base-repository.ts
+++ templates/opinionated/src/repositories/base-repository.ts
@@ -6,51 +6,70 @@ import { InMemoryDB } from "@providers/db-in-memory/db-in-memory.provider";
 
 @provide(BaseRepository)
 class BaseRepository<T extends IEntity> implements IBaseRepository<T> {
-    @inject(InMemoryDB) private inMemoryDB!: InMemoryDB;
+    @inject(InMemoryDB)
+    private inMemoryDB!: InMemoryDB;
+    private tableName: string;
 
-    protected get USERDB(): T[] {
-        return [...this.inMemoryDB.getUserDB()] as T[];
+    constructor(tableName: string) {
+        this.tableName = tableName;
+    }
+
+    protected get table(): T[] {
+        return [...this.inMemoryDB.getTable(this.tableName)] as T[];
     }
 
     create(item: T): T | null {
-        const existingItem = this.USERDB.find((user) => user.id === item.id);
+        const existingItem = this.table.find((i) => i.id === item.id);
         if (existingItem) {
             throw new Error(`Object with id ${item.id} already exists`);
         }
 
-        this.inMemoryDB.getUserDB().push(item);
+        this.inMemoryDB.getTable(this.tableName).push(item);
+
+        this.inMemoryDB.printTable(this.tableName);
         return item;
     }
 
     delete(id: string): boolean {
-        const db = this.inMemoryDB.getUserDB();
+        const db = this.inMemoryDB.getTable(this.tableName);
         const index: number = db.findIndex((item) => item.id === id);
 
         if (index !== -1) {
             db.splice(index, 1);
+
+            this.inMemoryDB.printTable(this.tableName);
+
             return true;
         }
         return false;
     }
 
     update(item: T): T | null {
-        const db = this.inMemoryDB.getUserDB();
+        const db = this.inMemoryDB.getTable(this.tableName);
         const index: number = db.findIndex((i) => i.id === item.id);
 
         if (index !== -1) {
             db[index] = item;
+
+            this.inMemoryDB.printTable(this.tableName);
+
             return item;
         }
         return null;
     }
 
     find(id: string): T | null {
-        const user = this.USERDB.find((item) => item.id === id);
-        return user || null;
+        const item = this.table.find((item) => item.id === id);
+
+        this.inMemoryDB.printTable(this.tableName);
+
+        return item || null;
     }
 
     findAll(): T[] {
-        return this.USERDB;
+        this.inMemoryDB.printTable(this.tableName);
+
+        return this.table;
     }
 }
 
