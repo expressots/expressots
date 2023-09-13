1b997b7b78fa22305de5755de76165d3d5ad0ddf
Richard Zampieri <br.developer@gmail.com>
2023-09-12 21:17:57 -0700

    Initial commit

---
+++ packages/adapter-express/.eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+module.exports = {
+	extends: [
+		"eslint:recommended",
+	 	"plugin:@typescript-eslint/recommended",
+		"eslint-config-prettier",
+		"prettier"
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: ["./tsconfig.cjs.json"]
+	},
+	plugins: ["@typescript-eslint"],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ["dist", "node_modules", ".eslintrc.cjs"],
+	rules: {
+		"@typescript-eslint/adjacent-overload-signatures": "error",
+		"@typescript-eslint/array-type": ["error", { default: "generic" }],
+		"@typescript-eslint/await-thenable": "error",
+		"@typescript-eslint/ban-types": "error",
+		"@typescript-eslint/class-literal-property-style": "error",
+		"@typescript-eslint/explicit-function-return-type": "error",
+	},
+}
