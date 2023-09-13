36d8dec0435a2456d1b9e1d5dca5bfb8956cfdea
Richard Zampieri <br.developer@gmail.com>
2023-09-13 02:44:38 -0700

    refactor: expose app base class for adapter

--- packages/core/src/application/index.ts
+++ packages/core/src/application/index.ts
@@ -1,3 +1,4 @@
 export * from "../adapter-express";
 export { AppContainer } from "./app-container";
 export { AppFactory } from "./application-factory";
+export { ApplicationBase } from "./application-base";
