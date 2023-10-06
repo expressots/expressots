af4cb01f4b562a8ae40592ec6ac668482163faa7
Richard Zampieri <br.developer@gmail.com>
2023-10-05 19:38:34 -0700

    fix: linter fix

--- packages/cli/src/@types/config.ts
+++ packages/cli/src/@types/config.ts
@@ -11,7 +11,7 @@ interface IProviders {
 		schemaPath: string;
 		entitiesPath: string;
 		entityNamePattern: string;
-	}
+	};
 }
 
 /**
--- packages/cli/src/providers/cli.ts
+++ packages/cli/src/providers/cli.ts
@@ -10,25 +10,24 @@ const generateProviders = (): CommandModule<CommandModuleArgs, any> => {
 		describe: "Scaffold a new provider",
 		aliases: ["a"],
 		builder: (yargs: Argv): Argv => {
-			yargs.positional("provider", {
-				choices: [
-					"prisma",
-				] as const,
-				describe: "The provider to add to the project",
-				type: "string",
-			})
-			.option("library-version", {
-				describe: "The library version to install",
-				type: "string",
-				default: "latest",
-				alias: "v",
-			})
-			.option("provider-version", {
-				describe: "The version of the provider to install",
-				type: "string",
-				default: "latest",
-				alias: "vp",
-			});
+			yargs
+				.positional("provider", {
+					choices: ["prisma"] as const,
+					describe: "The provider to add to the project",
+					type: "string",
+				})
+				.option("library-version", {
+					describe: "The library version to install",
+					type: "string",
+					default: "latest",
+					alias: "v",
+				})
+				.option("provider-version", {
+					describe: "The version of the provider to install",
+					type: "string",
+					default: "latest",
+					alias: "vp",
+				});
 
 			return yargs;
 		},
