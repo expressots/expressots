63e74771ec47a4fab45ff9bc04da7dee4b586573
Richard Zampieri <br.developer@gmail.com>
2024-03-29 15:32:35 -0700

    refactor: remove the cli version from info cmd

--- packages/cli/src/info/form.ts
+++ packages/cli/src/info/form.ts
@@ -3,26 +3,6 @@ import path from "path";
 import fs from "fs";
 import os from "os";
 import { printError } from "../utils/cli-ui";
-import { exec } from "child_process";
-import util from "util";
-
-const execPromisified = util.promisify(exec);
-
-async function getCLIVersion(): Promise<string> {
-	try {
-		const { stdout } = await execPromisified("npm list -g @expressots/cli");
-		const version = stdout;
-		const versionRegex = /@expressots\/cli@(\d+\.\d+\.\d+)/;
-		const versionMatch: string = version.match(versionRegex)?.[1];
-		return versionMatch;
-	} catch (error) {
-		printError(
-			"CLI version not available.",
-			"Unable to get the CLI version.",
-		);
-	}
-	return "";
-}
 
 function getInfosFromPackage() {
 	try {
@@ -39,8 +19,6 @@ function getInfosFromPackage() {
 		console.log(chalk.white(`\tDescription: ${packageJson.description}`));
 		console.log(chalk.white(`\tVersion: ${packageJson.version}`));
 		console.log(chalk.white(`\tAuthor: ${packageJson.author}`));
-
-		//getCLIVersion();
 	} catch (error) {
 		printError(
 			"No project information available.",
@@ -50,15 +28,11 @@ function getInfosFromPackage() {
 }
 
 const infoForm = (): void => {
-	const cliVersion = "1.6.0"; //await getCLIVersion();
 	getInfosFromPackage();
 
 	console.log(chalk.green("System information:"));
 	console.log(chalk.white(`\tOS Version: ${os.version()}`));
 	console.log(chalk.white(`\tNodeJS version: ${process.version}`));
-
-	console.log(chalk.green("CLI Version:"));
-	console.log(chalk.white(`\tCurrent version: v${cliVersion}`));
 };
 
 export { infoForm };
