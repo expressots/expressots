3fde40a8754e22ad377e58d2c5a0b16fcf116dd0
Richard Zampieri <br.developer@gmail.com>
2024-04-03 23:32:50 -0700

    refactor: appfactory class

--- packages/core/src/application/application-factory.ts
+++ packages/core/src/application/application-factory.ts
@@ -1,122 +1,52 @@
-import express from "express";
+import {
+  IWebServer,
+  IWebServerPublic,
+  IWebServerConstructor,
+} from "@expressots/adapter-express";
 import { Container } from "inversify";
 import { Logger } from "../provider/logger/logger.provider";
-import { AppExpress, IApplicationExpress } from "@expressots/adapter-express";
-//import { AppFastify } from "./fastify/application-fastify";
-//import { IApplicationFastify } from "./fastify/application-fastify.interface";
-//import { Handler } from "@fastify/middie";
+
+/**
+ * Type guard to check if input is a constructor type of IWebServer.
+ * @param input - Input to check.
+ * @returns True if input is a constructor type of IWebServer.
+ */
+function isWebServerConstructor<T extends IWebServer>(
+  input: unknown,
+): input is IWebServerConstructor<T> {
+  return input && typeof input === "function";
+}
 
 /**
  * AppFactory Class
  *
- * Responsible for creating an instance of the Application,
- * either using a custom application type or with provided middlewares.
+ * Responsible for creating an instance of the IWebServer implementation using a custom application type.
  */
 class AppFactory {
   public static container: Container;
   private static logger: Logger = new Logger();
-  /**
-   * Creates an instance of the application using a custom application type.
-   * @param container - InversifyJS container to resolve dependencies.
-   * @param CustomAppType - Custom application class extending Application.
-   * @returns Instance of the application.
-   */
-  public static async create(
-    container: Container,
-    CustomAppType: new () => AppExpress,
-    httpServerFactory?: new () => AppExpress,
-  ): Promise<IApplicationExpress>;
-  /* public static async create(
-    container: Container,
-    CustomAppType: new () => AppFastify,
-    httpServerFactory?: new () => AppFastify,
-  ): Promise<IApplicationFastify>; */
 
   /**
-   * Creates an instance of the application with provided middlewares.
+   * Create an instance of a web server.
    * @param container - InversifyJS container to resolve dependencies.
-   * @param middlewares - Array of Express middlewares to be applied.
-   * @returns Instance of the application.
+   * @param webServerType - Constructor of a class that implements IWebServer, or array of middlewares.
+   * @returns A promise that resolves to an instance of IWebServer.
    */
-  public static async create(
+  public static async create<T extends IWebServer>(
     container: Container,
-    middlewares: Array<express.RequestHandler>,
-    httpServerFactory?: new () => AppExpress,
-  ): Promise<AppExpress>;
-  /* public static async create(
-    container: Container,
-    middlewares: Array<express.RequestHandler>,
-    httpServerFactory?: new () => AppFastify,
-  ): Promise<AppFastify>; */
-
-  /**
-   * Implementation of the create method, handling both overloads.
-   * @param container - InversifyJS container to resolve dependencies.
-   * @param appTypeOrMiddlewares - Custom application class or array of middlewares.
-   * @returns Instance of the application.
-   */
-  public static async create(
-    container: Container,
-    appTypeOrMiddlewares:
-      | (new () => AppExpress) /*| AppFastify*/
-      | Array<express.RequestHandler>,
-    httpServerFactory?: new () => AppExpress /*| AppFastify*/,
-  ): Promise<
-    IApplicationExpress | /*IApplicationFastify |*/ AppExpress /*| AppFastify*/
-  > {
-    // Set the container for the application global access
+    webServerType: IWebServerConstructor<T>,
+  ): Promise<IWebServerPublic> {
+    // Set the container to the static property.
     AppFactory.container = container;
 
-    let app: AppExpress /*| AppFastify*/ = {} as AppExpress; /*| AppFastify*/
-
-    if (this.isOpinionated(appTypeOrMiddlewares)) {
-      switch (httpServerFactory) {
-        case AppExpress:
-          app = container.resolve(appTypeOrMiddlewares as new () => AppExpress);
-          app.create(container);
-          return app as IApplicationExpress;
-        /* case AppFastify:
-          app = container.resolve(appTypeOrMiddlewares as new () => AppFastify);
-          await app.create(container);
-          return app as IApplicationFastify; */
-        default:
-          app = container.resolve(appTypeOrMiddlewares as new () => AppExpress);
-          app.create(container);
-          return app as IApplicationExpress;
-      }
+    if (isWebServerConstructor<T>(webServerType)) {
+      const webServerInstance: T = new webServerType();
+      await webServerInstance.configure(container);
+      return webServerInstance;
     } else {
-      switch (httpServerFactory) {
-        case AppExpress:
-          app = container.get<AppExpress>(AppExpress);
-          app.create(
-            container,
-            appTypeOrMiddlewares as Array<express.RequestHandler>,
-          );
-          return app as AppExpress;
-        /* case AppFastify:
-          app = container.get<AppFastify>(AppFastify);
-          await app.create(container, appTypeOrMiddlewares as Array<Handler>);
-          return app as AppFastify; */
-        default:
-          app = container.get<AppExpress>(AppExpress);
-          app.create(
-            container,
-            appTypeOrMiddlewares as Array<express.RequestHandler>,
-          );
-          return app as AppExpress;
-      }
+      this.logger.error("Invalid web server type.", "app-factory:create");
+      throw new Error("Invalid web server type.");
     }
-  }
-
-  /**
-   * Checks if the provided parameter is a custom application type.
-   * @param appTypeOrMiddlewares - Custom application class or array of middlewares.
-   * @returns True if the provided parameter is a custom application type.
-   */
-  private static isOpinionated<T>(
-    appTypeOrMiddlewares: (new () => T) | Array<express.RequestHandler>,
-  ): boolean {
-    return typeof appTypeOrMiddlewares === "function";
   }
 }
 
