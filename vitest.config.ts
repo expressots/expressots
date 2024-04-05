78c19895c1ed441fb5eee255fd28f1639fa89989
Richard Zampieri <br.developer@gmail.com>
2024-04-04 22:03:56 -0700

    test: add unit test application, common, console

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 /**
  * @see {@link https://vitejs.dev/config/}
  * @see {@link https://vitest.dev/config/}
  */
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     exclude: [
@@ -12,18 +14,29 @@ export default defineConfig({
       "**/test/**",
       "**/benchmark/**",
       "templates/**",
+      "**/lib/**",
     ],
     coverage: {
       all: true,
       include: ["**/core/**"],
       exclude: [
         "**/node_modules/**",
+        "**/lib/**",
         "**/test/**",
         "**/benchmark/**",
         "**/index.ts/**",
         "templates/**",
       ],
+      thresholds: {
+        global: {
+          statements: 85,
+          branches: 85,
+          functions: 85,
+          lines: 85,
+        },
+      },
       reporter: ["text", "html", "json"],
+      provider: "v8",
     },
     // ref: https://vitest.dev/config/#testtimeout
     testTimeout: 10000,
