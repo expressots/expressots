9cef3c2cf6624d24eb82b70e87dd0a2ba4ed50f6
Richard Zampieri <br.developer@gmail.com>
2024-04-05 23:26:22 -0700

    feat: add codecov plugin bundler

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     tsconfigPaths(),
     codecovVitePlugin({
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "<bundle project name>",
+      bundleName: "expresso-ts-core-coverage",
       uploadToken: process.env.CODECOV_TOKEN,
     }),
   ],
