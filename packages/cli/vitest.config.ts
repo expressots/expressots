9d606a0ad0e9c341e62b47e4ca12ddacada6d621
Richard Zampieri <br.developer@gmail.com>
2024-04-28 04:25:02 -0700

    feat: add code coverage

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -7,45 +7,35 @@ import tsconfigPaths from "vite-tsconfig-paths";
  * @see {@link https://vitest.dev/config/}
  */
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    codecovVitePlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "expresso-ts-cli-coverage",
-      uploadToken: process.env.CODECOV_TOKEN,
-    }),
-  ],
-  test: {
-    globals: true,
-    environment: "node",
-    setupFiles: ["reflect-metadata"],
-    exclude: [
-      "**/node_modules/**",
-      "**/test/**",
-      "**/benchmark/**",
-      "**/bin/**",
-    ],
-    coverage: {
-      all: true,
-      include: ["./src/**"],
-      exclude: [
-        "**/node_modules/**",
-        "**/bin/**",
-        "**/test/**",
-        "**/index.ts/**",
-      ],
-      thresholds: {
-        global: {
-          statements: 85,
-          branches: 85,
-          functions: 85,
-          lines: 85,
-        },
-      },
-      reporter: ["text", "html", "json"],
-      provider: "v8",
-    },
-    // ref: https://vitest.dev/config/#testtimeout
-    testTimeout: 10000,
-  },
+	plugins: [
+		tsconfigPaths(),
+		codecovVitePlugin({
+			enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+			bundleName: "expresso-ts-cli-coverage",
+			uploadToken: process.env.CODECOV_TOKEN,
+		}),
+	],
+	test: {
+		globals: true,
+		environment: "node",
+		setupFiles: ["reflect-metadata"],
+		exclude: ["**/node_modules/**", "**/benchmark/**", "**/bin/**"],
+		coverage: {
+			all: true,
+			include: ["./src/**"],
+			exclude: ["**/node_modules/**", "**/bin/**", "**/index.ts/**"],
+			thresholds: {
+				global: {
+					statements: 85,
+					branches: 85,
+					functions: 85,
+					lines: 85,
+				},
+			},
+			reporter: ["text", "html", "json"],
+			provider: "v8",
+		},
+		// ref: https://vitest.dev/config/#testtimeout
+		testTimeout: 10000,
+	},
 });
