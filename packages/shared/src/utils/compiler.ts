4af679370629986b8579fde4780f88bdef54748c
Richard Zampieri <br.developer@gmail.com>
2024-10-19 17:37:16 -0700

    feat: add compiler and package definitions

---
+++ packages/shared/src/utils/compiler.ts
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { existsSync } from "node:fs";
+import path from "path";
+import { RegisterOptions, Service } from "ts-node";
+import chalk from "chalk";
+import { ExpressoConfig } from "../config";
+
+export function printError(message: string, component: string): void {
+  console.error(chalk.red(`${message}:`, chalk.bold(chalk.white(`[${component}] ❌`))));
+}
+
+/**
+ * The path to the expressots.config.ts file
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const EXPRESSOTS_CONFIG: string = path.join(process.cwd(), "expressots.config.ts");
+
+/**
+ * The config object
+ */
+let globalConfigObject: any = null;
+
+/**
+ * The ts-node register options
+ */
+const regOpt: RegisterOptions = {
+  compilerOptions: {
+    module: "commonjs",
+  },
+  moduleTypes: {
+    "**": "cjs",
+  },
+};
+
+/**
+ * Singleton compiler class
+ */
+export class Compiler {
+  private static instance: Compiler;
+
+  private constructor() {}
+
+  static get Instance(): Compiler {
+    if (!Compiler.instance) {
+      Compiler.instance = new Compiler();
+    }
+    return Compiler.instance;
+  }
+
+  async getService(): Promise<Service> {
+    const tsnode = await import("ts-node");
+    const compiler: Service = tsnode.register(regOpt);
+    return compiler;
+  }
+
+  private static interopRequireDefault(obj: any): { default: any } {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const module = require(obj);
+    return module && module.__esModule ? module : { default: module };
+  }
+
+  private static async findConfig(dir: string): Promise<string> {
+    const configPath = path.join(dir, "expressots.config.ts");
+    const exists = existsSync(configPath);
+
+    if (exists) return configPath;
+
+    const parentDir = path.join(dir, "..");
+
+    if (parentDir === dir) {
+      printError("No config file found!", "expressots.config.ts");
+      process.exit(1);
+    }
+
+    return Compiler.findConfig(parentDir);
+  }
+
+  static async loadConfig(): Promise<ExpressoConfig> {
+    const compiler: Service = await Compiler.Instance.getService();
+
+    compiler.enabled(true);
+
+    globalConfigObject = Compiler.interopRequireDefault(await Compiler.findConfig(process.cwd()));
+
+    compiler.enabled(false);
+
+    return globalConfigObject.default;
+  }
+}
