d1c5a2ebdd4b192f5e450a0f08fb84d219dbb32e
Richard Zampieri <br.developer@gmail.com>
2024-10-28 21:46:11 -0700

    fix: adjust env validator to load files dynamically

--- packages/core/src/provider/environment/env-validator.provider.ts
+++ packages/core/src/provider/environment/env-validator.provider.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { injectable } from "../../di/inversify";
 import { Logger } from "../logger/logger.provider";
 import { IProvider } from "../provider-manager";
-import { config } from "@expressots/shared";
+import { parse } from "@expressots/shared";
 
 type DefaultValueType = string | number | boolean | undefined;
 
@@ -13,11 +13,11 @@ type DefaultValueType = string | number | boolean | undefined;
  * @provide EnvValidatorProvider
  */
 @injectable()
-class EnvValidatorProvider implements IProvider {
+export class EnvValidatorProvider implements IProvider {
   name: string = "EnvValidatorProvider";
   version: string = "0.0.1";
   author: string = "Richard Zampieri";
-  repo: string = "internal";
+  repo: string = "https://github.com/expressots/expressots";
 
   private logger: Logger;
 
@@ -30,6 +30,7 @@ class EnvValidatorProvider implements IProvider {
    * @param key - The key of the environment variable.
    * @param defaultValue - The default value to return if the environment variable is not set.
    * @returns The value of the environment variable, or the default value if not set.
+   * @public API
    */
   public get(
     key: string,
@@ -41,23 +42,23 @@ class EnvValidatorProvider implements IProvider {
   /**
    * Validates and loads all environment variables from the .env file.
    * If the .env file does not exist or any environment variables are not set, the process will exit with an error.
+   * @param envFile -  
+   * @public API
    */
-  public checkAll(): void {
+  public checkFile(envFile: string): void {
     // Get the full path of the .env file
-    const envFilePath: string = path.join(process.cwd(), ".", ".env");
+    const envFilePath: string = path.join(process.cwd(), ".", envFile);
 
     // Check if the .env file exists
     if (!fs.existsSync(envFilePath)) {
       this.logger.error(
-        "Environment file .env is not defined.",
-        "env-validator-provider",
+        `Environment file [${envFile}] is not defined.`,
+        "environment-provider",
       );
       process.exit(1);
     }
 
-    // Load the environment variables from the .env file
-    const dotenvConfigOutput = config({ path: envFilePath });
-    const dotEnvParsed = dotenvConfigOutput.parsed;
+    const dotEnvParsed = parse(fs.readFileSync(envFilePath, "utf8"));
 
     /* Verify if all environment variables are defined */
     let hasError: boolean = false;
@@ -66,8 +67,8 @@ class EnvValidatorProvider implements IProvider {
         // Check if the environment variable is not defined or is an empty string
         if (!process.env[key] || process.env[key] === "") {
           this.logger.error(
-            `Environment variable ${key} is not defined.`,
-            "env-validator-provider",
+            `Environment variable [ ${key} ] is not defined.`,
+            "environment-provider",
           );
           hasError = true;
         }
@@ -110,5 +111,3 @@ String.prototype.AsNumber = function (): number | undefined {
 String.prototype.AsString = function (): string | undefined {
   return String(this);
 };
-
-export { EnvValidatorProvider };
