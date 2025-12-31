98a7119e6ff2baed38e69e6462430fbec486b0d7
Richard Zampieri <br.developer@gmail.com>
2025-12-30 22:58:49 -0800

    chore: update TypeScript configuration and ESLint settings
    - Added path mappings for useCases and controllers in tsconfig.build.json and tsconfig.json
    - Disabled the no-unused-vars rule in ESLint configuration for better flexibility

--- templates/application/eslint.config.mjs
+++ templates/application/eslint.config.mjs
@@ -26,6 +26,7 @@ export default tseslint.config(
             ],
             "@typescript-eslint/explicit-function-return-type": "off",
             "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
             "@typescript-eslint/no-non-null-assertion": "warn",
         },
     },
