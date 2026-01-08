0950caa29822b79a288c06c8e32e33d6cc9d13b3
Richard Zampieri <br.developer@gmail.com>
2026-01-08 15:14:19 -0800

    feat: add CI environment detection properties to IConsoleMessage interface

--- packages/shared/src/interfaces/console.interface.ts
+++ packages/shared/src/interfaces/console.interface.ts
@@ -6,4 +6,9 @@ export interface IConsoleMessage {
   appName: string;
   appVersion: string;
   apiVersions?: Array<string>; // Optional array of API versions detected from @Version() decorators
+  /** CI environment detection info (for bootstrap logging) */
+  ciDetection?: {
+    detected: boolean;
+    platform?: string;
+  };
 }
